53c5e650f1e6dcbe81884a0c4a68f6b0d7811d0c
satish <satishnsn91@gmail.com>
2024-06-20 17:16:53 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("camera")
+
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img =img.convert("L")
+    st.image(gray_img)
+    
